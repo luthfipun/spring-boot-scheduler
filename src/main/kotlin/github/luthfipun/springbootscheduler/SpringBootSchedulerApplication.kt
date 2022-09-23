7a10b20d223140b5cb72ca07bddea29a5edb7d6a
Luthfi Abdul Azis <luthfieabdulazis@gmail.com>
2022-09-23 13:46:11 +0700

    init

---
+++ src/main/kotlin/github/luthfipun/springbootscheduler/SpringBootSchedulerApplication.kt
@@ -0,0 +1,11 @@
+package github.luthfipun.springbootscheduler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootSchedulerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootSchedulerApplication>(*args)
+}
---
+++ src/test/kotlin/github/luthfipun/springbootscheduler/SpringBootSchedulerApplicationTests.kt
@@ -0,0 +1,13 @@
+package github.luthfipun.springbootscheduler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootSchedulerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
